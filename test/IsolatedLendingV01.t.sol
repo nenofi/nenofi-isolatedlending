5cb8a96d39b50449248bb169c14cfb535a408993
nenofi <nenofi@proton.me>
2022-10-28 16:38:13 +0700

    add asset implemented

--- test/IsolatedLendingV01.t.sol
+++ test/IsolatedLendingV01.t.sol
@@ -1,41 +1,52 @@
-// // SPDX-License-Identifier: MIT
-// pragma solidity >= 0.8.4;
+// SPDX-License-Identifier: MIT
+pragma solidity >= 0.8.4;
 
-// import "forge-std/Test.sol";
-// import "forge-std/console.sol";
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
 
-// import "../src/IsolatedLendingV01.sol";
-// import "../src/test/MockERC20.sol";
-// import "../src/interface/IOracle.sol";
+import "../src/IsolatedLendingV01.sol";
+import "../src/test/MockERC20.sol";
+import "../src/interface/IOracle.sol";
 
 
-// contract IsolatedLendingV01Test is Test {
-//     IsolatedLendingV01 public isolatedLending;
-//     MockERC20 public neIDR;
-//     MockERC20 public usdt;
-//     IOracle public neIDRusdt;
+contract IsolatedLendingV01Test is Test {
+    IsolatedLendingV01 public isolatedLending;
+    MockERC20 public neIDR;
+    MockERC20 public usdt;
+    IOracle public neIDRusdt;
 
-//     address public Alice = address(0x2);
+    address public Alice = address(0x2);
+    address public Bob = address(0x3);
+    address public Charlie = address(0x4);
 
-//     function setUp() public{
-//         neIDR = new MockERC20("neRupiah", "neIDR");
-//         usdt = new MockERC20("USD Tether", "USDT");
-//         isolatedLending = new IsolatedLendingV01(address(usdt), address(neIDR), address(neIDRusdt));
-//     }
 
-//     function testAddCollateral() public {
-//         console.log("HERE");
-//         console.log(address(this));
-//         vm.startPrank(Alice);
-//         usdt.mint(1000e18);
-//         console.log(usdt.balanceOf(address(Alice)));
-//         usdt.approve(address(isolatedLending), 1000e18);
-//         isolatedLending.addCollateral(address(Alice), 1000e18);
-//         vm.stopPrank();
-//     }
 
-//     function testAccrue() public{
-//         isolatedLending.accrue();
-//     }
+    function setUp() public{
+        neIDR = new MockERC20("neRupiah", "neIDR");
+        usdt = new MockERC20("USD Tether", "USDT");
+        isolatedLending = new IsolatedLendingV01(address(usdt), address(neIDR), address(neIDRusdt));
+    }
 
-// }
+    function testAddCollateral() public {
+        vm.startPrank(Alice);
+        usdt.mint(1000e18);
+        usdt.approve(address(isolatedLending), 1000e18);
+        isolatedLending.addCollateral(address(Alice), 1000e18);
+        vm.stopPrank();
+        assertEq(isolatedLending.userCollateralAmount(Alice), 1000e18);
+
+    }
+
+    function testAddAsset() public {
+        vm.startPrank(Bob);
+        neIDR.mint(10000000e18);
+        neIDR.approve(address(isolatedLending), 10000000e18);
+        isolatedLending.addAsset(address(Bob), 1000000e18);
+        vm.stopPrank();
+        console.log(isolatedLending.balanceOf(address(Bob)));
+        assertEq(isolatedLending.userCollateralAmount(Alice), 1000e18);
+    }
+
+
+
+}
